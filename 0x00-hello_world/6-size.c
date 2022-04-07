7efe92061dc3e30e58e1a3e56602cd153c69f792
damukuzi <damukuzi@gmail.com>
2022-04-07 10:33:46 -0700

    adding changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
 	printf("Size of a float: %lu byte(s) \n", sizeof(float));
-	printf(stderr, "Anything");
+	fprintf(stderr, "Anything");
 	return (0);
 }
