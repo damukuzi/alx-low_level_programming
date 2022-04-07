c7fde837e1e356990fb213027579902053d3a2d8
damukuzi <damukuzi@gmail.com>
2022-04-07 08:28:35 -0700

    adding readme

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
  * main - Short description, single line
- * @parameterx: Description of parameter x
-(* a blank line
+ 
  * Description: Longer description of the function)?
 (* section header: Section description)*
  * Return: Description of the returned value
