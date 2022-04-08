a3288d2965ebe1f6b45e5e8f9d136bff1579578a
damukuzi <damukuzi@gmail.com>
2022-04-08 09:57:46 -0700

    adding changes

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - prints alphabet
+ * Return: returns zero
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('$');
+	putchar('\n');
+	return (0);
+}
