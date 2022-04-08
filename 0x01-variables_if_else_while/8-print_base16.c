27be066733a0066e1827b114ab4d83ef5ad5e2b9
damukuzi <damukuzi@gmail.com>
2022-04-08 10:28:56 -0700

    adding changes

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - prints alphabet
+ * Return: returns zero
+ */
+int main(void)
+{
+	char ch;
+	int num
+
+	for (num = 0; ch <= 9; num++)
+	{
+		putchar(ch);
+	}
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
