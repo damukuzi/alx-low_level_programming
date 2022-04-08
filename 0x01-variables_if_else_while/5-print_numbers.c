bc3db6fc44fd61f8a37af942883f84996b63e4fe
damukuzi <damukuzi@gmail.com>
2022-04-08 10:19:02 -0700

    adding changes

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - prints alphabet
+ * Return: returns zero
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d", num)
+	}
+
+	putchar('\n');
+	return (0);
+}
