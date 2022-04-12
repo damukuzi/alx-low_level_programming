d378acd73eabac91005e776f57dd0aac19f74fd8
damukuzi <damukuzi@gmail.com>
2022-04-12 10:46:08 -0700

    adding changes

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -16,3 +16,22 @@ int _islower(int c)
 		return (0);
 	}
 }
+
+/**
+ * main - prints alphabet
+ * Return: returns zero
+*/
+
+int main(void)
+{
+    int r;
+
+    r = _islower('H');
+    _putchar(r + '0');
+    r = _islower('o');
+    _putchar(r + '0');
+    r = _islower(108);
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+}
