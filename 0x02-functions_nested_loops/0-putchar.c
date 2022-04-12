7b8074bcfb130251a7534687728a05d792a813e0
damukuzi <damukuzi@gmail.com>
2022-04-12 09:05:18 -0700

    adding changes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,5 +9,5 @@
  */
 int main(void)
 {
-	printf("_putchar")
+	printword("_putchar")
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * printword - prints alphabet
+ * @sentence: Description of parameter x
+ * Return: returns zero
+*/
+
+int printword(char *sentence)
+{
+	printf("%s", sentence);
+	return (0);
+}
+
+#endif
