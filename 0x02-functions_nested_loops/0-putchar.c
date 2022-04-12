edea22fe1a5b12ba962555be5091a195facff10b
damukuzi <damukuzi@gmail.com>
2022-04-12 08:02:27 -0700

    adding comments

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - prints alphabet
+ * Return: returns zero
+ */
+int main(void)
+{
+	printf("_putchar")
+}
