bf57f06b2a418af4634250bc51a6da6f5a51a8f6
damukuzi <damukuzi@gmail.com>
2022-04-12 11:43:12 -0700

    adding changes

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,16 +13,16 @@ void print_to_98(int n)
 		for (n = n; n < 98; n++)
 		{
 			printf("%d, ", n);
-			printf("%d\n", 98);
 		}
+		printf("%d\n", 98);
 	}
 	else
 	{
 		for (n = n; n > 98; n--)
 		{
-			printf("%d, ", n);
-			printf("%d\n", 98);
+			printf("%d, ",n);
 		}
+		printf("%d\n", 98);
 	}
 }
 
