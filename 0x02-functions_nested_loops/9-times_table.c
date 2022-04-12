4a43d8b7e255a3a85de43a76cdb9a7142f17cd1c
damukuzi <damukuzi@gmail.com>
2022-04-12 12:02:50 -0700

    adding changes

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,34 +17,28 @@ void times_table(void)
 		for (column = 0; column <= 9; column++)
 		{
 			value = row * column;
-				if (column == 0)
-				{
-					_putchar ('0');
-				}
-				if (column != 0)
-				{
-					if ((value / 10) != 0)
-					{
-						_putchar (' ');
-						_putchar ((value / 10) + '0');
-						_putchar ((value % 10) + '0');
-					}
-					else
-					{
-						_putchar (' ');
-						_putchar ((value % 10) + '0');
-					}
-				}
-				if (column < 9)
-				{
-					_putchar(',');
-					_putchar (' ');
-				}
-				else
+			if (column == 0)
+			{
+				_putchar ('0');
+			}
+			if (column != 0)
+			{
+				if ((value / 10) != 0)
 				{
 					_putchar ((value / 10) + '0');
 					_putchar ((value % 10) + '0');
 				}
+				else
+				{
+					_putchar (' ');
+					_putchar ((value % 10) + '0');
+				}
+			}
+			if (column < 9)
+			{
+				_putchar(',');
+				_putchar (' ');
+			}
 		}
 	_putchar ('\n');
 	}
