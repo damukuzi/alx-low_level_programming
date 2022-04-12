57f8594cbfb84083ad594490f8c43d3c02fa8190
damukuzi <damukuzi@gmail.com>
2022-04-12 09:48:58 -0700

    adding changes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,7 @@
 /* more headers goes there */
 #include "main.h"
-/**
- * main - prints alphabet
- * Return: returns zero
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
+
+
 
 void print_alphabet(void)
 {
@@ -20,4 +13,14 @@ void print_alphabet(void)
                 ch++;
         }
         putchar('\n');
+}
+
+/**
+ * main - prints alphabet
+ * Return: returns zero
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
 }
