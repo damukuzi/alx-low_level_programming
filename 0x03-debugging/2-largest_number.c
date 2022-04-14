8a4b6d9544bec8ae338ce65eb54efe2421a0f029
damukuzi <damukuzi@gmail.com>
2022-04-14 04:09:13 -0700

    adding changes

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,11 +16,11 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b >= a && a >= c)
+	if (b >= a && a >= c)
 	{
 		largest = b;
 	}
-	else
+	if (c >= a && c >= b)
 	{
 		largest = c;
 	}
