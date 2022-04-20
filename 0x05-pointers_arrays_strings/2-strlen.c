fbff6eec963b26f7a3f44a7eeb9654f3ebc132a1
damukuzi <damukuzi@gmail.com>
2022-04-20 17:47:38 +0300

    adding changes

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_strlen -length of a string
+ *@word: A pointer to an int that will be changed/updated
+ *
+ *Return: int with length of string
+ */
+
+int _strlen(char *word)
+{
+    int n;
+
+    n = 0;
+    while (word[u] != '\0')
+    {
+        n++;
+    }
+    return (n);
+}
