b9ade5c233cf573d87cfc99a8998ce23e3d3a296
damukuzi <damukuzi@gmail.com>
2022-05-03 07:43:30 -0700

    adding changes

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,7 +7,7 @@
  * check_num - check - string there are digit
  * @str: array str
  *
- * Return; Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int check_num(char *str)
@@ -33,7 +33,7 @@ int check_num(char *str)
  * @argc: Count arguments
  * @argv: Arguments
  *
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
@@ -46,7 +46,7 @@ int main(int argc, char *argv[])
 	count = 1;
 	while (count < argc) /*Goes through the whole array*/
 	{
-		if(check_num(argv[count]))
+		if (check_num(argv[count]))
 		{
 			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 			sum += str_to_int;
