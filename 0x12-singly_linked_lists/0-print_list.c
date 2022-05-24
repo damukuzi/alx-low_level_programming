40cf138bf8703ca9c8e7d5401a2a9fc5c363ee66
damukuzi <damukuzi@gmail.com>
2022-05-24 11:03:40 -0700

    adding changes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,13 +13,16 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
+		{
 			printf("[%d] %s\n", 0, "(nil)");
+		}
 		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-			nelem++;
 		}
+		h = h->next;
+		nelem++;
+		
 	}
 	return (nelem);
 }
